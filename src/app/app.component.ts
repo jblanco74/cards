e90df79b7a9b622302c45f957b69d575c693b8d9
Javier Blanco <jbits.solutions@gmail.com>
2022-05-31 10:37:11 +0100

    duplicate card and customise them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      imageUrl: 'assets/tree.jpeg',
+      title: 'Neat Tree',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { 
+      imageUrl: 'assets/mountain.jpeg',
+      title: 'Snowy Mountain',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    { 
+      imageUrl: 'assets/biking.jpeg',
+      title: 'Mountain Biking',
+      username: 'biking12222',
+      content: 'I did some biking today'
+    }
+  ];
 }
